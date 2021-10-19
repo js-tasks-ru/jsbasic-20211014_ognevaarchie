21bdca9bf560816fe351cf025c365e1e1f661f10
Ogneva Archie <ognevaarchie@gmail.com>
2021-10-20 01:34:03 +0300

    function sumSalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (const key in salaries) {
+    const element = salaries[key];
+    if (Number.isFinite(element)) {
+      sum += element;
+    }
+   
+  }
+  return sum;
 }
