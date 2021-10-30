6a45e03911d24da860efa598f3ec18572e5dd239
Ogneva Archie <ognevaarchie@gmail.com>
2021-10-30 21:48:49 +0300

    camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,16 @@
 function camelize(str) {
-  // ваш код...
+  let newArray = str.split('-').map((word, i) => {
+    if (i == 0) {
+      return word;
+    } else {
+      return word[0].toUpperCase() + word.slice(1); 
+    }
+    
+  });
+  return newArray.join('');
+
+
+ 
+
 }
+
