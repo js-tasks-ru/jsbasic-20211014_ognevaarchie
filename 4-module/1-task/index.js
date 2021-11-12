4eb69e01631064ecd6f0c786c497843d6408727c
Ogneva Archie <ognevaarchie@gmail.com>
2021-11-12 19:06:09 +0300

    makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ulElement = document.createElement('ul');
+  for (let index = 0; index < friends.length; index++) {
+    const element = friends[index];
+    let liElement = ulElement.appendChild(document.createElement('li'));
+    liElement.textContent = `${element.firstName} ${element.lastName}`
+  }
+  return ulElement
+ 
 }
